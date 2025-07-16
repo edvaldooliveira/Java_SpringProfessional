73571e7d443503c8d08a08753a0ba0d7ceb987fd
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-07-16 22:44:59 +0100

    Java Spring Professional

---
+++ JavaSpringProfessional/src/test/java/com/JavaSpring/JavaSpringProfessional/JavaSpringProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpring.JavaSpringProfessional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ JavaSpringProfessional/src/test/java/com/JavaSpring/JavaSpringProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
